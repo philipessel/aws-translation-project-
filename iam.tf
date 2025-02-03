f1cd82339a5afe32c93ff3b6794a672ae85bb942
Philip <philipessel2006@gmail.com>
2025-02-03 10:47:41 +0000

    modified iam.tf

--- iam.tf
+++ iam.tf
@@ -39,3 +39,10 @@ resource "aws_iam_role_policy_attachment" "s3_access" {
   role       = aws_iam_role.lambda_execution.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+# Attach the cloudwatch logs access managed policy:this will ensure that Lambda has the permission to write logs to CloudWatch.
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_access" {
+  role       = aws_iam_role.lambda_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
